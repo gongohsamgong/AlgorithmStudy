cd02309982c29725a13b12cded088d30748b08a9
Heewon Lee <heewonlee@ewhain.net>
2022-08-16 02:46:42 +0900

    [help] 0815

--- on/8958.py
+++ on/8958.py
@@ -1,6 +1,19 @@
 n = int(input())
 lst = []
+score = []
 
-for i in range (n):
+for i in range(n):
     lst = list(input())
-    print(lst)
+    a = 1
+    scr = 0
+    for j in range(len(lst) - 1):
+        if lst[j] == 'O':
+            scr += a
+            if lst[j+1] == 'O':
+                if a != 1:
+                    scr -= a
+                a += 1
+                scr += a
+            else:
+                a = 1
+    print(scr)
