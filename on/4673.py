645bde1b661b4f0f170cc61431d832bc27f4322a
Heewon Lee <heewonlee@ewhain.net>
2022-08-31 17:05:11 +0900

    [help] 4673

--- on/4673.py
+++ on/4673.py
@@ -1,14 +1,15 @@
 lst = [0] * 10001
 
-for i in range(10000):
-    if i in [1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97]:
-        lst[i] = 0
+def d(n):
+    if n in [1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97]:
+        n += 1
+        lst[n] = 1
     else:
-        lst[i] = 1
-        nxt = i + i // 1000 + i // 100 + i // 10 + i % 10
-        if nxt <= 10000:
-            lst[nxt] = 1
+        new = n % 1000 + n % 100 + n % 10
+        lst[n] = 0
+        d(new)
 
-for i in range(10000):
-    if lst[i] == 0:
+for i in range(10001):
+    if lst[i] == 1:
         print(i)
+
