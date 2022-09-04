5a556c970d10cafddc280d137fcf8f35a914f847
Heewon Lee <heewonlee@ewhain.net>
2022-09-04 15:54:29 +0900

    [fix] p46862 코드 수정

--- p46862.py
+++ p46862.py
@@ -24,3 +24,25 @@ def solution(n, lost, reserve):
             answer += 1
 
     return answer
+#
+# def solution(n, lost, reserve):
+#     answer = 0
+#     result = [1] * (n+2)
+#
+#     for l in lost:
+#         result[l] -= 1
+#     for r in reserve:
+#         result[r] += 1
+#
+#     for i in range(1, n+1):
+#         if result[i] == 0:
+#             if result[i-1] == 2:
+#                 result[i-1] = result[i] = 1
+#             elif result[i+1] == 2:
+#                 result[i+1] = result[i] = 1
+#
+#     for i in range(1, n+1):
+#         if result[i] > 0:
+#             answer += 1
+#
+#     return answer
