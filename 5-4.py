d9e1eb5125bd5deefbf0feb712a9bdb56b561e13
Heewon Lee <heewonlee@ewhain.net>
2022-11-02 17:07:20 +0900

    [solve] 5-3, 5-4

--- 5-4.py
+++ 5-4.py
@@ -1,23 +1,28 @@
-def dfs(x, y):
-  if x < 0 or x >= n or y < 0 or y >= m:
-    return False
-  if graph[x][y] == 0:
-    graph[x][y] = 1
-    dfs(x - 1, y)
-    dfs(x, y - 1)
-    dfs(x + 1, y)
-    dfs(x, y + 1)
-    return True
-  return False
+from collections import deque
 
-if __name__ == '__main__':
-  n, m = map(int, input().split())
-  graph = []
-  for i in range(n):
-    graph.append(list(map(int, input())))
-  
-  result = 0
-  for i in range(n):
-    for j in range(m):
-      if dfs(i, j) == True:
-        result += 1
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+  graph.append(list(map(int, input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+  queue = deque()
+  queue.append((x, y))  # 일단 지금 방문한 곳...을 큐에 넣는다
+  while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx < 0 or ny < 0 or nx >= n or ny >= m:
+        continue
+      if graph[nx][ny] == 0:
+        continue
+      if graph[nx][ny] == 1:
+        graph[nx][ny] = graph[x][y] + 1
+        queue.append((nx, ny))
+  return graph[n-1][m-1]
+
+print(bfs(0,0))
