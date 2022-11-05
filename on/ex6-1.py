811a75893658714460212d1a006a901615439b8f
Heewon Lee <heewonlee@ewhain.net>
2022-11-05 10:01:04 +0900

    [solving] 정렬 예제

---
+++ on/ex6-1.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i+1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        for j in range(i, 0, -1):
+            if array[j] < array[j-1]:
+                array[j], array[j-1] = array[j-1], array[j]
+            else:
+                break
+    return array
+
+
+if __name__ == '__main__':
+    read = sys.stdin.readline
+    arr = list(map(int, read().split()))
+    print(selection_sort(arr))
+    print(insertion_sort(arr))
