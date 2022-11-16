ac9b0e5c39eaa2085f4c5b561eed06d96f6bb6bf
Heewon Lee <heewonlee@ewhain.net>
2022-11-16 17:40:00 +0900

    [solve] 8-4

---
+++ 8-4.py
@@ -0,0 +1,16 @@
+import sys
+d = [0] * 1001
+
+
+def dynamic(n):
+    d[1] = 1
+    d[2] = 3
+    for i in range(3, n+1):
+        d[i] = d[i-1] + 2 * d[i-2]
+    return d[n]
+
+
+if __name__ == '__main__':
+    read = sys.stdin.readline
+    n = int(read())
+    print(dynamic(n) % 796796)
