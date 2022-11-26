8bdfbdd4b98c6885b4d1ac5cbc6d6c5531caaf5e
Heewon Lee <heewonlee@ewhain.net>
2022-11-26 10:34:19 +0900

    [solve] warmup 6

--- warmup_6.py
+++ warmup_6.py
@@ -1,22 +1,20 @@
 def solution(log):
-    answer = []
-    for i in range(len(log)):
-        if log[i] == '<':
-            if i == 0 or i == len(log) - 1:
-                continue
-            else:
-                answer.insert(len(answer) - 1, log[i])
-        elif log[i] == '>':
-            if i == 0 or i == len(log) - 1:
-                continue
-            else:
-                answer.insert(0, log[i])
-        elif log[i] == '-':
-            answer.pop()
+    answer = ''
+    front = list()
+    back = list()
 
-        answer.append(log[i])
+    for c in log:
+        if c == '-':
+            if front:
+                front.pop()
+        elif c == '<':
+            if front:
+                back.append(front.pop())
+        elif c == '>':
+            if back:
+                front.append(back.pop())
+        else:
+            front.append(c)
 
-    res = ''
-    for j in range(len(answer)):
-        res += str(answer[i])
-    return res
+    answer = ''.join(front) + ''.join(back[::-1])
+    return answer
