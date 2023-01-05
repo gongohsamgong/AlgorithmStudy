cb161dd66c155629e1d99fa8a1579a2755f7930e
Heewon Lee <heewonlee@ewhain.net>
2023-01-05 14:54:28 +0900

    [solve] week3 - 4
    프로그래머스 lv3 이중우선순위큐

---
+++ study/double_priority_queue.py
@@ -0,0 +1,25 @@
+def solution(operations):
+    answer = []
+    temp = []
+
+    for val in operations:
+        instruction, number = val.split(' ')
+        if instruction == "I":
+            temp.append(int(number))
+        else:
+            if len(temp) > 0:
+                if int(number) == 1:
+                    temp.pop()
+                elif number == '-1':
+                    temp.pop(0)
+            else:
+                continue
+        temp.sort()
+
+    if len(temp) == 0:
+        for i in range(2):
+            answer.append(0)
+    else:
+        answer.append(max(temp))
+        answer.append(min(temp))
+    return answer
