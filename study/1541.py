2fe114cd4321e0a19a964db0436151d66b4f3648
Heewon Lee <heewonlee@ewhain.net>
2023-01-30 00:25:57 +0900

    [solving] week7 - 1

--- study/1541.py
+++ study/1541.py
@@ -2,16 +2,16 @@ import sys
 
 
 if __name__ == '__main__':
-    read = sys.stdin.readline
-    a = read().split('-')
+    read = sys.stdin.readline   # 속도 개선을 위해 sys 사용   ex) 50-20+20-40+15
+    a = read().split('-')   # 값이 최소가 되기 위해서는 무조건 - 기준으로 괄호를 묶으면 됨. 따라서 - 기준으로 분리   ex) a = ['50', '20+20', '40+15']
     num = []
     for i in a:
         cnt = 0
-        s = i.split('+')
+        s = i.split('+')    # ex) i = ['50']    i = ['20', '20']    i = ['40', '15']
         for j in s:
-            cnt += int(j)
-        num.append(cnt)
-    n = num[0]
+            cnt += int(j)   # ex) cnt = 50      cnt = 40        cnt = 55
+        num.append(cnt) # ex) append(50), append(40), append(55)
+    n = num[0]  # ex) 50
     for i in range(1, len(num)):
-        n -= num[i]
+        n -= num[i] # ex) 50 - 40 - 55  for문 돌아가며 차례로 진행
     print(n)
---
+++ study/kakaoblind2023_personalinfo.py
@@ -0,0 +1,53 @@
+def add_date(term_date, today, months):  # 약관날짜, 오늘날짜, 계약기간
+    t_y = int(today.split('.')[0])  # today's year
+    t_m = int(today.split('.')[1])
+    t_d = int(today.split('.')[2])
+    p_y = int(term_date.split('.')[0])
+    p_m = int(term_date.split('.')[1])
+    p_d = int(term_date.split('.')[2])
+    res_d = t_d
+    if t_m + months > 12:
+        res_m = t_m
+        res_y = t_y + 1
+    else:
+        res_m = t_m + months
+        res_y = t_y
+    # result = str(res_y) + '.' + str(res_m) + '.' + str(res_d)
+    if res_y < t_y:
+        return 'expire'
+    elif res_y > t_y:
+        return 'keep'
+    else:  # res_y == t_y
+        if res_m < t_m:
+            return 'expire'
+        elif res_m > t_m:
+            return 'keep'
+        else:  # res_m == t_m
+            if res_d <= t_d:
+                return 'expire'
+            else:
+                return 'keep'
+
+
+def solution(today, terms, privacies):
+    answer = []
+    n = len(privacies)
+    t_y = today.split('.')[0]  # today's year
+    t_m = today.split('.')[1]
+    t_d = today.split('.')[2]
+    for i in range(n):
+        privacy_date = privacies[i].split('')[0]
+        privacy_term = privacies[i].split('')[1]
+        p_y = privacy_date.split('.')[0]
+        p_m = privacy_date.split('.')[1]
+        p_d = privacy_date.split('.')[2]
+        for i in range(len(terms)):
+            term = terms[i].split('')
+            if privacy_term == term[0]:
+                result = add_date(privacy_date, today, term[1])
+                if result == 'expire':
+                    answer.append(i + 1)
+                else:
+                    continue
+
+    return answer
