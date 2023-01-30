24a2160a68232803c31da020a7deec37f8d6e51d
Heewon Lee <heewonlee@ewhain.net>
2023-01-31 00:47:17 +0900

    [solving] week7 - 1
    2023 kakao blind recruitment 개인정보 수집 유효기간

--- study/kakaoblind2023_personalinfo.py
+++ study/kakaoblind2023_personalinfo.py
@@ -1,5 +1,5 @@
-def add_date(term_date, today, months): # 약관날짜, 오늘날짜, 계약기간
-    t_y = int(today.split('.')[0])   # today's year
+def add_date(term_date, today, months):  # 약관날짜, 오늘날짜, 계약기간
+    t_y = int(today.split('.')[0])  # today's year
     t_m = int(today.split('.')[1])
     t_d = int(today.split('.')[2])
     p_y = int(term_date.split('.')[0])
@@ -7,48 +7,40 @@ def add_date(term_date, today, months): # 약관날짜, 오늘날짜, 계약기�
     p_d = int(term_date.split('.')[2])
     res_d = t_d
     if t_m + int(months) > 12:
-        res_m = t_m
+        res_m = t_m + int(months) - 12
         res_y = t_y + 1
     else:
         res_m = t_m + int(months)
         res_y = t_y
-    # result = str(res_y) + '.' + str(res_m) + '.' + str(res_d)
+
     if res_y < t_y:
         return 'expire'
     elif res_y > t_y:
         return 'keep'
-    else:   # res_y == t_y
+    else:  # res_y == t_y
         if res_m < t_m:
             return 'expire'
         elif res_m > t_m:
             return 'keep'
-        else:   # res_m == t_m
+        else:  # res_m == t_m
             if res_d <= t_d:
                 return 'expire'
             else:
                 return 'keep'
 
-            
+
 def solution(today, terms, privacies):
     answer = []
-    n = len(privacies)
-    t_y = today.split('.')[0]   # today's year
-    t_m = today.split('.')[1]
-    t_d = today.split('.')[2]
-    for i in range(n):
+    for i in range(len(privacies)):
         privacy_date = privacies[i].split(' ')[0]
         privacy_term = privacies[i].split(' ')[1]
-        p_y = privacy_date.split('.')[0]
-        p_m = privacy_date.split('.')[1]
-        p_d = privacy_date.split('.')[2]
         for j in range(len(terms)):
             term = terms[j].split(' ')
             if privacy_term == term[0]:
                 result = add_date(privacy_date, today, term[1])
-                print('result', result)
                 if result == 'expire':
-                    answer.append(i+1)
+                    answer.append(i + 1)
                 else:
                     continue
-    
+
     return answer
