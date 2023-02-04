9d9e73353ac1b688cb06aac75ae2646461e04c22
Heewon Lee <heewonlee@ewhain.net>
2023-02-04 23:01:56 +0900

    [solve] week7 - 2

--- study/2583.py
+++ study/2583.py
@@ -16,7 +16,7 @@ def bfs(graph, x, y):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
-                graph[nx][ny] == 1
+                graph[nx][ny] = 1
                 queue.append((nx, ny))
                 size += 1
     return size
