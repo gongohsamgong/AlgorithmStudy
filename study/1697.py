4557fc74d8384bb1187ead3c8c2bdbfea38a1bec
Heewon Lee <heewonlee@ewhain.net>
2023-02-04 23:24:31 +0900

    [solve] week7 - 3
    백준 1697

--- study/1697.py
+++ study/1697.py
@@ -1,7 +1,8 @@
 from collections import deque
 import sys
 
-MAX = 10*5
+MAX = 100001
+
 
 def bfs(dist, n, k):
     q = deque()
@@ -11,13 +12,14 @@ def bfs(dist, n, k):
         if x == k:
             return dist[x]
         for nx in (x-1, x+1, x*2):
-            if 0<=nx<=MAX and dist[nx] == 0:
+            if 0 <= nx < MAX and dist[nx] == 0:
                 dist[nx] = dist[x] + 1
                 q.append(nx)
+
 
 if __name__ == '__main__':
     read = sys.stdin.readline
     n, k = map(int, read().split())
+    dist = [0] * MAX
     ans = bfs(dist, n, k)
     print(ans)
-    
