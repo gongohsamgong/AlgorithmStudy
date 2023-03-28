14a0d918ef07930d2ffb58de2e6dcbc98354a776
Heewon <heewon223@gmail.com>
2023-03-28 23:43:55 +0900

    [solving] 3-5

---
+++ book_codingTest/ch3/3-5.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def solution(n, m, arr):
+    total = n * (n-1) / 2
+    arr.sort()
+    for i in range(n):
+        
+    exception =
+    
+    
+    return total - exception
+
+
+if __name__ == '__main__':
+    read = sys.stdin.readline
+    N, M = map(int, read().split()) # N: 볼링공의 개수, M: 공의 최대 무게
+    array = list(map(int, read().split()))
