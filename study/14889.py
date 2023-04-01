50c501da3d329589b0fde3bea0535a887b00822c
Heewon <heewon223@gmail.com>
2023-04-01 19:20:53 +0900

    [solve] week13 - 2

--- study/14889.py
+++ study/14889.py
@@ -1,39 +1,43 @@
 import sys
-# https://www.acmicpc.net/problem/14889
 
-def calculate(n, ability, team1):
-    team2 = list(set(range(n))-set(team1))
-    team1_sum = 0
-    for i, v1 in enumerate(team1[:-1]):
-        for v2 in team1[i+1:]:
-            team1_sum += ability[v1][v2] + ability[v2, v1]
-    
-    team2_sum = 0
-    for i, v1 in enumerate(team2[:-1]):
-        for v2 in team2[i+1:]:
-            team2_sum += ability[v1][v2] + ability[v2][v1]
-    
-    ans.append(abs(team1_sum - team2_sum))
-    return ans
+def cal_diff(team1, team2, num):
+    sum_team1 = 0
+    sum_team2 = 0
 
-def solution(n, ans):
-   if len(ans) == n/2:
-       calculate(n, ability, ans)
+    for i in range(num):
+        for j in range(num):
+            sum_team1 += arr[team1[i]][team1[j]]
+            sum_team2 += arr[team2[i]][team2[j]]
 
-    for i in range(team[-1] if team else 0, n):
-        if team and team[0] != 0:
-            return None
-        if i not in team:
-            team.append(i)
-            calculate(n, ability, team, )
-            team.pop()
+    return abs(sum_team1 - sum_team2)
+
+
+def make_team(team1, count, n, start):
+    global answer
+
+    if count == n//2:
+        team2 = []
+        for i in range(n):
+            if i not in team1:
+                team2.append(i)
+
+        diff = cal_diff(team1, team2, count)
+        answer = min(answer, diff)
+        return
+
+    for i in range(start, n):
+        if i not in team1:
+            team1.append(i)
+            make_team(team1, count+1, n, i+1)
+            team1.remove(i)
 
 
 if __name__ == '__main__':
     read = sys.stdin.readline
     N = int(read())
-    ability = [list(map(int, read().split())) for _ in range(N)]
-    ans = []
-    answer = solution(N, ans)
+    arr = [list(map(int, read().split())) for _ in range(N)]
+
+    answer = int(1e9)
+    make_team([], 0, N, 0)
     print(answer)
     
