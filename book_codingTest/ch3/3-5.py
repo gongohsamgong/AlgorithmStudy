c96c68b255c83f89c9e84e5637bfe9bafc0ee6ce
Heewon <heewon223@gmail.com>
2023-04-03 23:40:17 +0900

    [solve] 3-5

--- book_codingTest/ch3/3-5.py
+++ book_codingTest/ch3/3-5.py
@@ -2,25 +2,18 @@ import sys
 from collections import Counter
 
 
-def solution(n, m, arr):
-    total = n * (n - 1) / 2
-    counter_arr = Counter(arr)
-    set_arr = list(set(arr))
-    
-    # for i in range(n):
-    #
-    # exception =
-    
-    # return total - exception
+def solution(n, m, weight):
+    result = 0
+    for i in range(1, m+1):
+        n -= weight[i]
+        result += weight[i] * n
+    return result
 
 
 if __name__ == '__main__':
     read = sys.stdin.readline
-    N, M = map(int, read().split())  # N: 볼링공의 개수, M: 공의 최대 무게
+    N, M = map(int, read().split()) # N: 볼링공의 개수, M: 공의 최대 무게
     array = list(map(int, read().split()))
-    _list = Counter(array)
-    print(_list[3])
-    sset = set(array)
-    print(list(sset)[0])
-    # ans = solution(N, M, array)
-    # print(ans)
+    weight = Counter(array)
+    ans = solution(N, M, weight)
+    print(ans)
