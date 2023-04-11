f593f3841f67b80eb0097744d7650aec5bb2cd17
Heewon <heewon223@gmail.com>
2023-04-11 23:07:56 +0900

    [solve] 2217

--- 2217.py
+++ 2217.py
@@ -2,12 +2,18 @@ import sys
 
 
 def solution(n, array):
-    weight = 0
-    
-    return weight
+    candidate = []
+    array.sort()
+    for weight in array:
+        candidate.append(weight * n)
+        n -= 1
+
+    return max(candidate)
 
 
 if __name__ == '__main__':
     read = sys.stdin.readline
-    N = int(read())
-    ropes = [int(read()) for _ in range(N)]
+    N = int(read()) # 줄 개수
+    ropes = [int(read()) for _ in range(N)] # 감당가능한 최대 중량
+    ans = solution(N, ropes)
+    print(ans)
