c7c823352d8d04949cd1253541cf0a2544babe99
Heewon <heewon@incross.com>
2023-04-13 09:32:31 +0900

    [solving] ex5-11

--- book_codingTest/ch5/ex5-11.py
+++ book_codingTest/ch5/ex5-11.py
@@ -3,6 +3,7 @@ from collections import deque
 
 def solution(n, m, array):
     # 0은 괴물있는 부분. 0 피해야댐
+    cnt = 0
     queue = deque()
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
@@ -13,9 +14,12 @@ def solution(n, m, array):
                 array[i][j] = 0
                 while queue:
                     x, y = queue.popleft()
+                    cnt += 1
                     for k in range(4):
                         nx = x + dx[k]
                         ny = y + dy[k]
+                        if nx == n and ny == m:
+                            return cnt
                         if 0 <= nx < m and 0 <= ny < n and array[nx][ny] == 1:
                             queue.append((nx, ny))
 
@@ -24,5 +28,5 @@ if __name__ == "__main__":
     read = sys.stdin.readline
     N, M = map(int, read().split())
     maze = [list(map(int, read().rstrip())) for _ in range(N)]
-    ans = solution(maze)
+    ans = solution(N, M, maze)
     print(ans)
