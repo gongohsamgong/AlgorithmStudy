552d6cfb4ae84e497160121eb3f37f57c1497529
Heewon <heewon223@gmail.com>
2023-04-28 00:40:55 +0900

    [solve] week16 - 1

--- study/goorm_goormSquare.py
+++ study/goorm_goormSquare.py
@@ -2,16 +2,19 @@ import sys
 
 
 def solution(n, arr):
-    count = []
-    arr.sort(key=lambda x: x[0])
-
-    return max(count)
+    count = 1
+    arr.sort(key=lambda x: (x[1], x[0]))
+    end_time = arr[0][1]
+    for i in range(1, n):
+        if arr[i][0] > end_time:
+            count += 1
+            end_time = arr[i][1]
+    return count
 
 
 if __name__ == "__main__":
     read = sys.stdin.readline
     N = int(read())
     timeline = [list(map(int, read().split())) for _ in range(N)]
-    # print(timeline)
     ans = solution(N, timeline)
     print(ans)
