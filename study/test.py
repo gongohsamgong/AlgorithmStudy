29ac7ba9f6ba65613f823103518e0f4ef4ff313c
Heewon <heewon@incross.com>
2023-05-02 13:05:47 +0900

    Auto stash before merge of "work" and "origin/work"

--- study/test.py
+++ study/test.py
@@ -1,3 +1,14 @@
-graph = [[1, 4], [1, 3], [2, 6], [2, 4]]
-graph.sort(key=lambda x: x[1])
-print(graph)
+# graph = [[1, 4], [1, 3], [2, 6], [2, 4]]
+# graph.sort(key=lambda x: x[1])
+# print(graph)
+
+from PIL import Image
+
+# open image file
+image = Image.open("C:/Users/intern/Downloads/ddd.jpg")
+
+# resize image to 140x180 pixels
+resized_image = image.resize((3900, 3900))
+
+# save resized image to file
+resized_image.save("C:/Users/intern/Downloads/hi.jpg")
