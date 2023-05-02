dfe065dbc274daaf22f96934287124eede73c1f4
Heewon <heewon223@gmail.com>
2023-05-02 23:21:37 +0900

    [solving] 4949

--- 4949.py
+++ 4949.py
@@ -3,38 +3,36 @@ import sys
 
 def solution(sentence):
     stack = []
-    for i in range(len(sentence)):
-        # print(stack)
-        if sentence[i] == '(':
-            stack.append('(')
-        elif sentence[i] == '[':
-            stack.append('[')
-        elif sentence[i] == ')' and len(stack) != 0:
-            if stack[-1] == '(':
+    for letter in sentence:
+        if letter == '(' or letter == '[':
+            stack.append(letter)
+        elif letter == ')':
+            if len(stack) != 0 and stack[-1] == '(':
                 stack.pop()
             else:
-                return 'no'
-        elif sentence[i] == ']' and len(stack) != 0:
-            if stack[-1] == '[':
+                stack.append(')')
+        elif letter == ']':
+            if len(stack) != 0 and stack[-1] == '[':
                 stack.pop()
             else:
-                return 'no'
-        else:
-            continue
+                stack.append(']')
     if len(stack) == 0:
-        return 'yes'
+        print('yes')
     else:
-        return 'no'
+        print('no')
 
 
 if __name__ == "__main__":
     read = sys.stdin.readline
     ans = []
     while True:
-        sentence = read()
+        sentence = read().rstrip()
         # print(sentence)
         if sentence == '.':
             break
-        print(solution(sentence))
-        ans.append(solution(sentence))
-    print(*ans)
+        # print(solution(sentence))
+        else:
+            solution(sentence)
+        # ans.append(solution(sentence))
+        # print(ans)
+    # print(*ans)
