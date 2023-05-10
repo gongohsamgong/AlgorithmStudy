82bb4083d34634adcff04d55da33ebe83c3efc62
Heewon <heewon223@gmail.com>
2023-05-10 23:56:17 +0900

    [solve] ex7-2, ex7-3

--- book_codingTest/ch7/ex7-2.py
+++ book_codingTest/ch7/ex7-2.py
@@ -9,10 +9,11 @@ def binary_search(array, target, start, end):
     elif array[mid] < target:
         return binary_search(array, target, mid+1, end)
 
+
 n, target = list(map(int, input().split()))
 array = list(map(int, input().split()))
 result = binary_search(array, target, 0, n-1)
-if result == None:
+if result is None:
     print("원소가 존재하지 않습니다.")
 else:
     print(result + 1)
