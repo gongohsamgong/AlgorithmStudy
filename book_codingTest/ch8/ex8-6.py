5699d1f3899b5fc3b3c6ec6d59273f4c69e578f6
Heewon <heewon223@gmail.com>
2023-05-12 01:39:28 +0900

    [retry] ex8-6

--- book_codingTest/ch8/ex8-6.py
+++ book_codingTest/ch8/ex8-6.py
@@ -1,15 +1,18 @@
 import sys
 
 
-def solution(n, k):
-    food = 0
+def solution(n, array):
+    d = [0] * 100
+    d[0] = array[0]
+    d[1] = max(array[0], array[1])
+    for i in range(2, n):
+        d[n-1] = max(d[n-2], d[n-3] + array[n-1])
+    return d[n-1]
 
-    return food
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     read = sys.stdin.readline
     N = int(read())
-    K = list(map(int, read().split()))
-    ans = solution(N, K)
+    foods = list(map(int, read().split()))
+    ans = solution(N, foods)
     print(ans)
