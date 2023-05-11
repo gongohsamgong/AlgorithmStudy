e6c515b9e2c96e10292b783fa8441e0d2f90c06f
Heewon <heewon@incross.com>
2023-05-11 17:38:05 +0900

    [solving] 7795

--- study/7795.py
+++ study/7795.py
@@ -2,28 +2,20 @@ import sys
 
 
 def solution(n, m, a, b):
-    count = 0
-    b.sort()
+    max_b = max(b)
+    cnt = 0
     for i in range(n):
-        if a[i] > max(b):
-            count += m
-            continue
-        for j in range(m):
-            if a[i] > b[j]:
-                count += 1
-            else:
-                break
-    return count
+        if a[i] > max_b:
+            cnt += m
+        else:
 
 
 if __name__ == "__main__":
     read = sys.stdin.readline
-    answers = []
     T = int(read())
     for _ in range(T):
         N, M = map(int, read().split())
-        A_size = list(map(int, read().split()))
-        B_size = list(map(int, read().split()))
-        answers.append(solution(N, M, A_size, B_size))
-    for answer in answers:
-        print(answer)
+        A = list(map(int, read().split()))
+        B = list(map(int, read().split()))
+        B.sort()
+        solution(N, M, A, B)
