dbb2f47c2915873bd7056903aaf0cc7ba7670c63
Heewon <heewon@incross.com>
2023-05-22 13:46:28 +0900

    [solving] 9663
    시간초과

--- on/9663.py
+++ on/9663.py
@@ -1,37 +1,29 @@
 import sys
 
 
-def solution(n, array):
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if array[i][j] == 0:
-                array[i][j] = 1 # set queen
-
-    return cnt
+def adjacent(x, n):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
 
 
-def place_queen(x, y, n, array):
-    if array[y][x] == 0:
-        array[y] = [-1] * n
-        array[x] = [-1] * n
-        while True:
-            new_x = x - 1
-            new_y -= 1
-            if new_x < 0 or new_x >= n or new_y < 0 or new_y >= n:
-                break
+def dfs(x, n):
+    global count
 
-
-        array[y][x] = 1 # set queen
-
-
-
-def count_queen(array):
-
+    if x == n:
+        count += 1
+    else:
+        for i in range(n):
+            row[x] = i
+            if adjacent(x, n):
+                dfs(x+1, n)
 
 
 if __name__ == "__main__":
     read = sys.stdin.readline
     N = int(read())
-    board = [[0]*N for _ in range(N)]
-    solution(N, board)
+    row = [0] * N
+    start, count = 0, 0
+    dfs(start, N)
+    print(count)
