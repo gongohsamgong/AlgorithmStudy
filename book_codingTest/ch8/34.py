7d919521d8d42356f82e6ea273bb7563b1829857
Heewon <heewon223@gmail.com>
2023-06-02 00:11:55 +0900

    [solve] 34

--- book_codingTest/ch8/34.py
+++ book_codingTest/ch8/34.py
@@ -13,9 +13,19 @@ def solution(n, array):
 """
 
 
+def solution(n, array):
+    dp = [1] * n
+    for i in range(1, n):
+        for j in range(0, i):
+            if array[j] < array[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return n - max(dp)
+
+
 if __name__ == "__main__":
     read = sys.stdin.readline
     N = int(read())
     power = list(map(int, read().split()))
+    power.reverse()
     ans = solution(N, power)
     print(ans)
