346d012ac2ae3abced4fa6e87920e7290b938c94
Heewon <heewon223@gmail.com>
2023-09-08 13:11:32 +0900

    [solve] BJ 2563

---
+++ BJ/2563.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def solution(number, paper_location):
+    total = 0
+    array = [[0] * 100 for _ in range(100)]
+    for loc in paper_location:
+        for i in range(loc[1], loc[1] + 10):
+            for j in range(loc[0], loc[0] + 10):
+                array[i][j] = 1
+    for k in range(100):
+        total += array[k].count(1)
+    return total
+
+
+if __name__ == "__main__":
+    read = sys.stdin.readline
+    paper_number = int(read())
+    paper_locations = []
+    for _ in range(paper_number):
+        paper_locations.append(list(map(int, read().split())))
+    answer = solution(paper_number, paper_locations)
+    print(answer)
