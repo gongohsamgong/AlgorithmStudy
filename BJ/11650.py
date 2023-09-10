70e5773116fbcb80755e655dd308d8cf51cf5df0
Heewon <heewon223@gmail.com>
2023-09-10 17:21:57 +0900

    [solve] BJ 11650

--- BJ/11650.py
+++ BJ/11650.py
@@ -1,17 +1,12 @@
 import sys
 
 
-def solution(array):
-    array.sort(key=lambda x: (x[1], x[0]))
-    return array
-
-
 if __name__ == "__main__":
     read = sys.stdin.readline
     N = int(read())
     dots = []
     for _ in range(N):
         dots.append(list(map(int, read().split())))
-    result = solution(dots)
+    result = sorted(dots)
     for x, y in result:
         print("%d %d" % (x, y))
