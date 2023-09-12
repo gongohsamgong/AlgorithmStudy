2b7717b5ec5a009716ab2cba3fc93a9030a5cc63
Heewon <heewon223@gmail.com>
2023-09-12 10:28:57 +0900

    [solve] BJ 18870

--- BJ/1427.py
+++ BJ/1427.py
@@ -3,6 +3,7 @@ import sys
 
 if __name__ == "__main__":
     read = sys.stdin.readline
+    # numbers = sorted(list(map(int, read().rstrip())), reverse=True)
     numbers = list(map(int, read().rstrip()))
     numbers.sort(reverse=True)
     for i in range(len(numbers)):
