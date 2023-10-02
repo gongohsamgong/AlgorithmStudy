b3da9bec67b58d49d96f0196748aaeb5d564a41e
Heewon <heewon223@gmail.com>
2023-10-02 16:00:00 +0900

    [solve] BJ 25421

--- BJ/25421.py
+++ BJ/25421.py
@@ -2,23 +2,14 @@ import sys
 
 
 def solution(n):
-    result = 0
-    if n == 1:
-        least, most = 1, 9
-    else:
-        least, most = 2, 0
-        for i in range(n-1):
-            least += 9 * 10 ** i
-        for j in range(n):
-            most += 9 * 10 ** j
-    for num in range(least, most + 1):
-        result = result + 1 % 987654321
-        str_num = str(num)
-        for digit in range(n-1):
-            if '0' in str_num or abs(int(str_num[digit]) - int(str_num[digit + 1])) > 2:
-                result -= 1
-                break
-    return result
+    num = [1 for _ in range(9)]
+    while n != 1:
+        temp = []
+        for i in range(9):
+            temp.append(sum(num[max(0, i-2): min(10, i+3)]) % 987654321)
+        num = temp
+        n -= 1
+    return sum(num) % 987654321
 
 
 if __name__ == "__main__":
