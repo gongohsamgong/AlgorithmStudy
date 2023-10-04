38b5b2fcda9f5fae805d2ec431db9d269af04c8b
Heewon <heewon223@gmail.com>
2023-10-04 16:55:43 +0900

    [retry] BJ 7490
    better solution using product from itertools

--- BJ/7490.py
+++ BJ/7490.py
@@ -1,5 +1,6 @@
 import copy
 import sys
+from itertools import product
 
 
 def recur(arr, n):
@@ -20,11 +21,28 @@ def recur(arr, n):
     arr.pop()
 
 
+def product_solution(n):
+    result = []
+    for insert in product(['+', '-', ' '], repeat=n-1):
+        num = 1
+        expression = ''
+        for sign in insert:
+            expression += str(num) + sign
+            num += 1
+        expression += str(n)
+
+        if eval(expression.replace(' ', '')) == 0:
+            result.append(expression)
+
+    return sorted(result)
+
+
 if __name__ == "__main__":
     read = sys.stdin.readline
     T = int(read())
     for _ in range(T):
         N = int(read())
+        '''        
         operators = []
         recur([], N-1)
         for operator in operators:
@@ -34,4 +52,8 @@ if __name__ == "__main__":
             statement += str(N)
             if eval(statement.replace(' ', '')) == 0:
                 print(statement)
+        '''
+        answer_arrays = product_solution(N)
+        for answer in answer_arrays:
+            print(answer)
         print()
