010a3afb5de2edf3b5b30a3118d59b3229c65208
Heewon <heewon223@gmail.com>
2023-10-24 18:53:51 +0900

    [solve] 프로그래머스 힙
    더 맵게 재시도

--- programmers/더맵게.py
+++ programmers/더맵게.py
@@ -1,18 +1,13 @@
 import heapq
 
+
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
-
     while scoville[0] < K:
-        mixed = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
-        heapq.heappush(scoville, mixed)
-        answer += 1
-        if len(scoville) == 1 and scoville[0] < K:
+        if len(scoville) == 1:
             return -1
+        new_scoville = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
     return answer
-
-
-scoville = [1, 2, 3, 9, 10, 12]
-K = 7
-print(solution(scoville, K))
